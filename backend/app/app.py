8ca8d5bc0adeb6cb7e03ef925c20a2a0a95c52df
milankwiatkowski <milan.mikolaj.kwiatkowski@gmail.com>
2025-10-15 00:20:55 +0200

    Started working on basic endpoints, focusing on users logging-in. Also added a surprise for my colleagues :).

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, send_file
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -7,6 +7,31 @@ CORS(app)
 @app.route('/message', methods=['GET'])
 def message():
     return jsonify({"message": "Don't honk in the woods"}), 200
+
+@app.route('/login',methods=['POST'])
+def login():
+    if(request.args.get("username")=="Admin") and (request.args.get("password")=="12345"):
+        return jsonify({"message":"Witaj, "+request.args.get("username"),"AccessLevel":"Admin"})
+    elif(request.args.get("username")=="User") and (request.args.get("password")=="12345"):
+        return jsonify({"message":"Witaj, "+request.args.get("username"),"AccessLevel":"User"})
+    else:
+        return jsonify({"message":"Nieprawidlowy login lub haslo"})
+
+@app.route('/getUserData', methods=['GET'])
+def getUserData():
+    return jsonify({"AccessLevel":"User","Username":"Jelen"})
+
+@app.route('/getDBData',methods=["GET"])
+def getDayData():
+    return jsonify({"DBData":"TODO"})
+
+@app.route('/getCameraImage',methods=['GET'])
+def getCameraImage():
+    return jsonify({"CamImage":"TODO"})
+
+@app.route('/dhitw',methods=['GET'])
+def dhitw():
+    return send_file("./public/sarnaLICENCJAT.png", mimetype='image/png')
 
 if __name__ == '__main__':
     app.run(port=5000)
