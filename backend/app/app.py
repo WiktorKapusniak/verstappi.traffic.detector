25870f7620e31d9e3b00a69f21ecc0d27006233a
milankwiatkowski <milan.mikolaj.kwiatkowski@gmail.com>
2025-10-17 15:43:30 +0200

    Added basic backend support for keycloak. Yet to add password+secret hashing and prepare docker for keycloak

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,12 +1,41 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, send_file
 from flask_cors import CORS
-
+import keycloak_config
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/message', methods=['GET'])
 def message():
     return jsonify({"message": "Don't honk in the woods"}), 200
+
+@app.route('/login',methods=['POST'])
+def login():
+    try:
+        data = request.json()
+        username = data.get("username")
+        password = data.get("password")
+        token = keycloak_config.keycloak_openid.token(username, password)
+        user_id = keycloak_config.keycloak_admin.get_user_id(request.args.get("username"))
+        realm_roles = keycloak_config.keycloak_admin.get_realm_roles_of_user(user_id)
+        return jsonify({"message":"Witaj, "+request.args.get("username"),"AccessLevel":realm_roles[0],"token":token})
+    except:
+        return jsonify({"message":"Nieprawidlowy login lub haslo"})
+
+@app.route('/getUserData', methods=['GET'])
+def getUserData():
+    return jsonify({"AccessLevel":"User","Username":"Jelen"})
+
+@app.route('/getDBData',methods=["GET"])
+def getDayData():
+    return jsonify({"DBData":"TODO"})
+
+@app.route('/getCameraImage',methods=['GET'])
+def getCameraImage():
+    return jsonify({"CamImage":"TODO"})
+
+@app.route('/dhitw',methods=['GET'])
+def dhitw():
+    return send_file("./public/sarnaLICENCJAT.png", mimetype='image/png')
 
 if __name__ == '__main__':
     app.run(port=5000)
---
+++ backend/app/keycloak_config.py
@@ -0,0 +1,17 @@
+from keycloak import KeycloakAdmin, KeycloakOpenID
+
+keycloak_admin = KeycloakAdmin(
+    server_url="http://localhost:8080/",
+    username="admin",
+    password="dUdjas890J!m!djaSDUdasou@",
+    realm_name="RealtimeTraffic",
+    client_id="ClientNo01",
+    client_secret_key="pbDHzsyKV0nNVh1VzzVRcCnP7R64FX28",
+    verify=True)
+
+keycloak_openid = KeycloakOpenID(
+    server_url="http://localhost:8080/auth/",
+    client_id="ClientNo01",
+    realm_name="RealtimeTraffic",
+    client_secret_key="pbDHzsyKV0nNVh1VzzVRcCnP7R64FX28"
+)
