bd905ed39dd0c97557728dbd24ef012f69f0988e
Wiktor Koncewicz <wiktor.koncewicz31a@gmail.com>
2025-10-20 20:32:21 +0200

    finish Makefile and workflow, allow pushing dev version of image

--- Makefile
+++ Makefile
@@ -1,2 +1,7 @@
+DOCKER_USER=underwoodsteam
+IMAGE=$(DEPLOYMENT)-$(END)
+
 build:
-	docker build -t kkk_user/$IMAGE-deployment $IMAGE/
+	docker build -t $(DOCKER_USER)/$(IMAGE):latest $(END)/
+	docker login -u $(DOCKER_USER) -p $(DOCKER_TOKEN)
+	docker push $(DOCKER_USER)/$(IMAGE):latest
---
+++ run-makefile.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Run: ./run-makefile.sh <frontend|backend>
+
+if [ $# -ne 1 ]; then
+    echo "Error: Specifying frontend or backend is required."
+    echo "Usage: $0 <frontend|backend>"
+    exit 1
+fi
+
+if [ "$1" != "frontend" ] && [ "$1" != "backend" ]; then
+    echo "Error: Invalid argument. Please specify 'frontend' or 'backend'."
+    exit 1
+fi
+
+read -s -p "Enter your password: " PASSWORD
+echo \n
+
+make build -e DEPLOYMENT=dev -e END=$1 -e DOCKER_TOKEN=$PASSWORD
