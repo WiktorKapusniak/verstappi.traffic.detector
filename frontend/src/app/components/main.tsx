0552a34213a6e6b3212007514f3e246bbe630c59
Wiktor Koncewicz <wiktor.koncewicz31a@gmail.com>
2025-10-29 20:40:34 +0100

    React apostrophe escape

--- frontend/src/app/components/main.tsx
+++ frontend/src/app/components/main.tsx
@@ -5,7 +5,7 @@ export default function Main() {
     const router = useRouter();
     return (
     <main>
-      <div>Don't honk in the woods</div>
+      <div>Don&apos;t honk in the woods</div>
       <button type="button" onClick={() => router.push("/login")}>Move to login</button>
     </main>
   );
